a8b60dc19c49f6c8283b50d4f153d1e695f90a2b
JamesBrel <jamesbrel119@gmail.com>
2022-05-13 18:44:34 +0000

    name change

--- Export All Groups with Their Layers.jsx
+++ Export All Groups with Their Layers.jsx
@@ -1,45 +1,116 @@
-function main() {
-  var outputFolder = Folder.selectDialog("Select destination folder");
-  var nameGroupList = [];
+//* ---------------------- VARIABLES ----------------------
+var outputFolder = Folder.selectDialog("Select destination folder");
+var nameGroupList = [];
+var operatingSystem = File.fs;
 
-  if (outputFolder !== null) {
-    alert("The export will take a few minutes, so just wait 😉", "Waiting");
-    var groupsList = app.activeDocument.layerSets;
-    resetLayers(groupsList);
+//* ---------------------- LOGIC CODE ----------------------
+if (outputFolder !== null) {
+  alert("The export will take a few minutes, so just wait 😉", "Waiting");
+  var groupsList = app.activeDocument.layerSets;
+  resetLayers(groupsList);
 
-    for (var groupIndex = 0; groupIndex < groupsList.length; groupIndex++) {
-      groupsList[groupIndex].visible = true;
-      var nameGroup = groupsList[groupIndex].name;
-      nameGroupList.push(nameGroup);
-      createFolder(outputFolder + "/" + nameGroup + "/");
-    }
-
-    for (var indexName in nameGroupList) {
-      groupsList[indexName].visible = true;
-      var layersList = groupsList[indexName].layers;
-
-      for (var indexLayer = 0; indexLayer < layersList.length; indexLayer++) {
-        if (layersList[indexLayer].visible == false) {
-          layersList[indexLayer].visible = true;
-          var layerName = layersList[indexLayer].name;
-
-          SaveImage(
-            outputFolder +
-              "/" +
-              nameGroupList[indexName] +
-              "/" +
-              layerName +
-              ".png",
-          );
-          layersList[indexLayer].visible = false;
-        }
-      }
-      groupsList[indexName].visible = false;
-    }
-
-    alert("The Export process is Done 😃", "Done");
-    openFolder(outputFolder);
+  for (var groupIndex = 0; groupIndex < groupsList.length; groupIndex++) {
+    groupsList[groupIndex].visible = true;
+    var nameGroup = groupsList[groupIndex].name;
+    nameGroupList.push(nameGroup);
+    createFolder(outputFolder + "/" + nameGroup + "/");
   }
+
+  for (var indexName in nameGroupList) {
+    groupsList[indexName].visible = true;
+    var layersList = groupsList[indexName].layers;
+
+    for (var indexLayer = 0; indexLayer < layersList.length; indexLayer++) {
+      if (layersList[indexLayer].visible == false) {
+        layersList[indexLayer].visible = true;
+        var layerName = layersList[indexLayer].name;
+
+        switch (operatingSystem) {
+          case "Windows":
+            windows(layerName, indexName);
+            break;
+          case "Macintosh":
+            macintosh(layerName, indexName);
+            break;
+          case "Unix":
+            unix(layerName, indexName);
+            break;
+          default:
+            break;
+        }
+
+        layersList[indexLayer].visible = false;
+      }
+    }
+    groupsList[indexName].visible = false;
+  }
+
+  alert("The Export process is Done 😃", "Done");
+  openFolder(outputFolder);
+}
+
+//* ---------------------- FUNCTIONS ----------------------
+
+function windows(_layerName, _indexName) {
+  if (hasWhiteSpace(_layerName)) {
+    var file = new File(
+      outputFolder +
+        "/" +
+        nameGroupList[_indexName] +
+        "/" +
+        "temp_name" +
+        ".png",
+    );
+    SaveImage(file);
+    file.rename(_layerName + ".png");
+  } else {
+    var file = new File(
+      outputFolder +
+        "/" +
+        nameGroupList[_indexName] +
+        "/" +
+        _layerName +
+        ".png",
+    );
+    SaveImage(file);
+  }
+}
+
+function macintosh(_layerName, _indexName) {
+  if (filename.length > 27 && hasWhiteSpace(_layerName)) {
+    var file = new File(
+      outputFolder +
+        "/" +
+        nameGroupList[_indexName] +
+        "/" +
+        "temp_name" +
+        ".png",
+    );
+    SaveImage(file);
+    file.rename(_layerName + ".png");
+  } else {
+    var file = new File(
+      outputFolder +
+        "/" +
+        nameGroupList[_indexName] +
+        "/" +
+        _layerName +
+        ".png",
+    );
+    SaveImage(file);
+  }
+  //* --- Photoshop on Mac limits the length of a File object's file name to 31 characters --- .
+}
+
+function unix(_layerName, _indexName) {
+  /**
+   ** Ici tu ajoute les lignes de code qui géront le
+   ** cas du système Unix..
+   */
+}
+
+function hasWhiteSpace(_nameLayer) {
+  return /\s/g.test(_nameLayer);
 }
 
 function openFolder(_folderPath) {
@@ -47,16 +118,15 @@ function openFolder(_folderPath) {
   folderCreated.execute();
 }
 
-function SaveImage(_data) {
-  var saveFile = File(_data);
+function SaveImage(_file) {
   var sfwOptions = new ExportOptionsSaveForWeb();
   sfwOptions.format = SaveDocumentType.PNG;
   sfwOptions.PNG8 = false;
   sfwOptions.includeProfile = false;
   sfwOptions.transparency = true;
-  sfwOptions.interlaced = 0;
+  sfwOptions.interlaced = false;
   sfwOptions.quality = 100; //0-100
-  activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, sfwOptions);
+  activeDocument.exportDocument(_file, ExportType.SAVEFORWEB, sfwOptions);
 }
 
 function resetLayers(_groups) {
@@ -72,5 +142,3 @@ function createFolder(_path) {
   var createFolder = new Folder(_path);
   if (!createFolder.exists) createFolder.create();
 }
-
-main();
